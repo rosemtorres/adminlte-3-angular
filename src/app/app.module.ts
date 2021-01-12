e6cb92fafcc5d7aaf0049e521850ee7559c32e5b
rosemtorres <roselyn.m.torres@gmail.com>
2021-01-12 08:06:34 +0800

    Adding Forms Module and converting tabs to spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { HeaderComponent } from './pages/main/header/header.component';
 import { FooterComponent } from './pages/main/footer/footer.component';
 import { MenuSidebarComponent } from './pages/main/menu-sidebar/menu-sidebar.component';
 import { BlankComponent } from './views/blank/blank.component';
-// import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ProfileComponent } from './views/profile/profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './pages/register/register.component';
@@ -51,7 +50,7 @@ registerLocaleData(localeEn, 'en-EN');
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // FormsModule,
+    FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
